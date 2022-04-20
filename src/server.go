d1258311061679ea7fa608ff245c4f8558fd7cce
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-19 20:08:54 -0400

    server now runs with a grql terminal to create queries

--- src/server.go
+++ src/server.go
@@ -1,61 +1,32 @@
 package main
 
 import (
-	"fmt"
-	// "net/http"
-	"encoding/json"
+	"log"
+	"net/http"
 	"os"
-
+	// "fmt"
+	// "encoding/json"
 	// "strconv"
-	"io/ioutil"
+	// "io/ioutil"
+	"github/Diogenesoftoronto/golang-api/graph"
+	"github/Diogenesoftoronto/golang-api/graph/generated"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-type ServerConfig struct {
-	Greetings greetings `json:"greetings"`
-	Users     []User    `json:"users"`
-}
-
-type greetings struct {
-	Languages []Language `json:"languages"`
-}
-
-type Language struct {
-	Lang string `json:"language"`
-	Message string `json:"message"`
-}
-
-type User struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Age    int    `json:"age"`
-	Social string `json:"social"`
-}
-
-type Social struct {
-	Facebook string `json:"facebook"`
-	Twitter  string `json:"twitter"`
-}
+const defaultPort = "8080"
 
 func main() {
-	// open and destructure the opened file into the jsonFIle and error value
-	jsonFile, err := os.Open("server_config.json")
-	// if the there error is present  print an error message
-	if err != nil {
-		fmt.Printf("Error opening server_config.json")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
-	// other wise
-	fmt.Println("server_config.json successfully opened")
 
-	defer jsonFile.Close()
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	byteJson, _ := ioutil.ReadAll(jsonFile)
-	// need to initialize the value that will store the json file to be unmarshalled
-	var serverConfig ServerConfig
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
-	json.Unmarshal(byteJson, &serverConfig)
-	// func greeting(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, greeting)
-	// }
-
-	fmt.Printf("hell is here: %v", serverConfig.Greetings.Languages[len(serverConfig.Greetings.Languages)-1].Lang)
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
