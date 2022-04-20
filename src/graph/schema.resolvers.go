01757e4d19406c755c2c4937cbd61eaebf4dc17c
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-19 20:10:22 -0400

    functions that query and mutate data

---
+++ src/graph/schema.resolvers.go
@@ -0,0 +1,39 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"math/rand"
+	"context"
+	"fmt"
+	"github/Diogenesoftoronto/golang-api/graph/generated"
+	"github/Diogenesoftoronto/golang-api/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo :=  &model.Todo{
+		Text: input.Text,
+		ID: fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{
+			ID: input.UserID, 
+			Name: "user "+input.UserID},
+		}
+		r.todos = append(r.todos, todo)
+		return todo, nil
+	// panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+	// panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
